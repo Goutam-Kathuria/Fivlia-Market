d3769abf1a0c3f5db2309cf4c336127d19e2114e
Goutam <goutam.kathuria@gmail.com>
2026-03-10 11:30:03 +0530

    build

--- controlers/authControler.js
+++ controlers/authControler.js
@@ -153,3 +153,22 @@ exports.editProfile = async (req, res) => {
     return res.status(500).json({ message: "Server error" });
   }
 };
+
+exports.forgotPassword = async (req, res) => {
+  try {
+    const { mobileNumber, newPassword } = req.body;
+
+    const user = await User.findOne({ mobileNumber });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    } 
+    
+    user.password = newPassword;
+    await user.save();
+
+    return res.status(200).json({ message: "Password updated successfully" });
+  } catch (error) {
+    console.error("Error updating password:", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
